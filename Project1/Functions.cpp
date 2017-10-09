dcd96a82f950e9c44b607d3b8534b895cb2a16a9
Severiano Badajoz <sevebadajoz@gmail.com>
2017-10-09 13:34:39 -0700

    Complete insertion operator and copy constructor functions

--- Project1/Functions.cpp
+++ Project1/Functions.cpp
@@ -35,10 +35,87 @@
 
 #include "DoublyList.h"
 
-// overloaded insertion operator
+// Overloaded insertion operator
+ostream& operator<<(ostream& out, const DoublyList& d) {
+	if (d.count > 0)
+	{
+		Node * curr = d.first;
+		while (curr != nullptr) 
+		{
+			out << curr->getData() << " "; 
+			curr = curr->getNext();
+		}
+	}
+	else
+	{
+		cerr << "List is empty.";
+	}
+	return out;
+}
 
+// Copy constructor
+DoublyList::DoublyList(const DoublyList& other) {
+	first = nullptr;
+	last = nullptr;
+	count = other.count;
+	Node * curr = other.first;
+	while(curr != nullptr) {
+		insertBack(curr->getData());
+		curr = curr->getNext();
+	}
+}
 
-// copy constructor
-
-
-// overloaded assignment operator
+// Overloaded Assignment operator
+DoublyList& DoublyList::operator=(const DoublyList& rightSide) {
+	if (&rightSide != this)
+	{
+		Node * rightCurr = rightSide.first;
+		Node * curr = first;
+		if (count == 0) {
+			while (curr != nullptr) {
+				insertBack(rightCurr->getData());
+				rightCurr = rightCurr->getNext();
+			}
+		}
+		else if (rightSide.count == 0) destroyList();
+		else if (rightSide.count < count) 
+		{
+			while (rightCurr != nullptr)
+			{
+				curr->setData(rightCurr->getData());
+				rightCurr = rightCurr->getNext();
+				curr = curr->getNext();
+			}
+			last = curr->getPrev();
+			last->setNext(nullptr);
+			Node * temp = curr;
+			curr = curr->getNext();
+			while (curr != nullptr)
+			{
+				delete temp;
+				temp = curr;
+				curr = curr->getNext();
+			}
+			count = rightSide.count;
+		}
+		else if (rightSide.count > count) {
+			first->setData(rightCurr->getData());
+			curr = curr->getNext();
+			rightCurr = rightCurr->getNext();
+			while (curr != nullptr) {
+				curr->setData(rightCurr->getData());
+				rightCurr = rightCurr->getNext();
+				curr = curr->getNext();
+			}
+			while (rightCurr != nullptr) {
+				insertBack(rightCurr->getData());
+				rightCurr = rightCurr->getNext();
+			}
+		}
+		count = rightSide.count;
+	}
+	else
+		cerr << "Attempted assignment to itself" << endl;
+	
+	return *this;
+}
