d832ff8c6df8fc7ff198d3de2542283f32ff2a99
Severiano Badajoz <sevebadajoz@gmail.com>
2017-10-09 11:38:29 -0700

    Initial commit

---
+++ Project1/Functions.cpp
@@ -0,0 +1,44 @@
+/*
+	Name header
+*/
+
+/*
+	After you completed the implementation, answer the following questions.
+
+	1) Why are the parameter lists in the testCopyConstructor and 
+	   testOverloadedAssignment functions passed by VALUE and not reference?
+	   
+	   Answer:
+
+
+	2) When the function testOverloadedAssignment is called, why is being 
+	   re-directed to the copy constructor? 
+
+	   Answer:
+
+
+	3) When the function testOverloadedAssignment is called, why is the list on
+	   the right of the assignment operator being checked before the list 
+	   on the left?
+
+	   Answer:
+
+
+	4) The first testing case for the overloaded assignment operator compares 
+	   a list to itself (line 6). Shouldn't an error message be displayed? 
+	   Why is it not displaying?
+
+	   Answer:
+
+
+*/
+
+#include "DoublyList.h"
+
+// overloaded insertion operator
+
+
+// copy constructor
+
+
+// overloaded assignment operator
